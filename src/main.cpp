e6168983dc77d9c0beacd9cf2aecbd0176126293
Ambi <github@alayna.dev>
2026-03-02 20:52:03 -0500

    Create SFML window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/VideoMode.hpp>
 #include <iostream>
+
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char* argv[])
 {
+  sf::RenderWindow window =
+    sf::RenderWindow(
+      sf::VideoMode({ 800, 600 }),
+      "test"
+    );
+
   std::cout << "Hello world!" << std::endl;
+
+  while (window.isOpen())
+  {
+    while (const std::optional event = window.pollEvent())
+    {
+      if (event->is<sf::Event::Closed>())
+        window.close();
+    }
+
+    window.clear(sf::Color::Magenta);
+    window.display();
+  }
 
   return 0;
 }
